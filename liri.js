523754e874a81a2f37c9fd40753137f203f3335e
Willa Jin <jin.willa@gmail.com>
2019-01-17 00:58:48 -0800

    finished basic requirements for liri.js

--- liri.js
+++ liri.js
@@ -16,6 +16,7 @@ var fs = require("fs");
 var input = process.argv;
 // liri command
 var liriCommand = process.argv[2];
+
 // combine liri command input with spaces
 var liriInput = "";
 for (var i = 3; i < input.length; i++) {
@@ -25,11 +26,7 @@ console.log(liriInput);
 
 
 // user command
-if (liriCommand === "concert-this") {
-    concertThis(liriInput);
-}
-
-else if (liriCommand === "spotify-this-song") {
+if (liriCommand === "spotify-this-song") {
     spotifySong(liriInput);
 }
 
@@ -42,36 +39,102 @@ else if (liriCommand === "do-what-it-says") {
 }
 
 
-// concert-this //
-// search Bands in Town Artist Events API for artist and display event information
-function concertThis(artist) {
+// spotify-this-song
+function spotifySong(song) {
+    var search = "";
+    // if no song is provided then default to "The Sign" by Ace of Base
+    if (song === "") {
+        search = "The Sign Ace of Base";
+    }
 
-    // replace spaces with +
-    artist = artist.split(" ").join("+");
+    else {
+        search = song;
+    }
 
-    // request to Bands in Town API with artist input
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
-        var data = JSON.parse(body);
-        
-        // if request is successful
-        if (!error && response.statusCode === 200) {
-            // display event information
-            console.log();
-          }
+    spotify.search({ type: 'track', query: song, limit: 3 }, function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+
+        // display song information - song, artist, album, preview link
+        else {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                console.log("//////////////////////////////////////////" +
+                    "\nSong: " + data.tracks.items[i].name +
+                    "\nArtist: " + data.tracks.items[i].artists[0].name +
+                    "\nAlbum: " + data.tracks.items[i].album.name +
+                    "\nPreview Link: " + data.tracks.items[i].preview_url +
+                    "\n");
+            }
+        }
     });
 }
 
-// spotify-this-song
-function spotifySong(song) {
-
-}
 
 // movie-this
 function movieThis(movie) {
+    var search = "";
+    // if no movie is provided then default to "Mr. Nobody"
+    if (movie === "") {
+        search = "Mr. Nobody";
+    }
 
+    else {
+        search = movie;
+    }
+
+    // combine search with "+"
+    search = search.split(" ").join("+");
+
+    request("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy", function (err, response, body) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
+
+        // display movie information - title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
+        else {
+            var data = JSON.parse(body);
+            console.log("//////////////////////////////////////////" + 
+            "\nMovie Title: " + data.Title + 
+            "\nRelease Year: " + data.Released + 
+            "\nIMDB Rating: " + data.imdbRating + 
+            "\nRotten Tomatoes Rating: " + data.tomatoRating + 
+            "\nCountry of Production: " + data.Country + 
+            "\nLanguage: " + data.Language + 
+            "\nPlot: " + data.Plot + 
+            "\nActors: " + data.Actors + 
+            "\n");
+        }
+    });
 }
+
 
 // do-what-it-says
 function doWhat() {
+    // read random.txt file with command
+    fs.readFile("./random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log("Error occurred: " + err);
+        }
 
+        else {
+            // split LIRI command and input
+            var liriString = data.split(",");
+            var command = liriString[0].trim();
+            var input = liriString[1].trim();
+
+            // pass command/input to functions
+            if (command === "spotify-this-song") {
+                spotifySong(input);
+            }
+            
+            else if (command === "movie-this") {
+                movieThis(input);
+            }
+            
+            else if (command === "do-what-it-says") {
+                doWhat();
+            }
+        }
+    });
 }
